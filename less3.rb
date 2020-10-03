9fd3bcc7d9e065652e8f75b37de2fe51f72631fc
Ilia gred <iliagred@gmail.com>
2020-10-03 08:56:19 +0300

    Initial commit

---
+++ less3.rb
@@ -0,0 +1,21 @@
+#first_number = 2308
+#second_number = 13
+#puts ("Первое число:" + first_number.to_s)
+#puts ("Второе число: " + second_number.to_s)
+
+#puts ("Среднее: " + ((first_number+second_number)/2).to_s)
+
+
+#if (first_number > second_number)
+#  puts ("Наибольшее число:" + first_number.to_s)
+#elsif (second_number > first_number)
+#  ("Наибольшее число:" + second_number.to_s)		  	
+#else
+#  puts 'они равны!'
+#end
+
+if rand(2) == 1
+  puts 'Выпала решка'
+else
+  puts 'Выпал орел'
+end
---
+++ money_convertor.rb
@@ -0,0 +1,24 @@
+puts "Какая у вас на руках валюта?"
+puts "  1. Рубли "
+puts "  2. Доллары"
+
+user_chouse = gets.chomp
+#puts user_chouse.class
+if user_chouse=="2"
+  puts "Сколько сейчас стоит 1 доллар?"
+  dollars_price=gets.to_f
+
+  puts "Сколько у вас долларов?"
+  dollars_count=gets.to_f
+  final_sum=(dollars_price * dollars_count).to_i
+  puts "Ваши запасы на сегодня равны: " + final_sum.to_s + "руб."
+else
+  puts "Сколько сейчас стоит 1 рубль?"
+  rub_price = gets.to_f
+  puts "Сколько у вас рублей?"
+  rub_count = gets.to_f
+  final_sum=(rub_price * rub_count).to_i
+  puts "Ваши запасы на сегодня равны: " + final_sum.to_s + "долларов."
+end
+
+#puts "Ваши запасы на сегодня равны: " + final_sum.to_s + "руб."
