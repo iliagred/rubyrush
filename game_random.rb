dd531047b93a21731c6e9955464e215c85e716af
Ilia gred <iliagred@gmail.com>
2020-10-04 08:15:31 +0300

    game random

--- game_random.rb
+++ game_random.rb
@@ -1,1 +1,50 @@
 
+puts "Загадано число от 0 до 16, отгадайте какое?"
+specific_number=rand(0-16)
+user_answer1=gets.to_i
+
+if user_answer1==specific_number
+  puts "Ура, вы выиграли!"
+end
+
+if user_answer1<specific_number
+  puts "Тепло (нужно больше)"
+
+user_answer1=gets.to_i
+end
+
+
+if user_answer1<specific_number
+  puts "Тепло (нужно больше)"
+
+  user_answer1=gets.to_i
+end
+if user_answer1<specific_number
+  puts "Тепло (нужно больше)"
+
+  user_answer1=gets.to_i
+end
+
+if user_answer1>specific_number
+  puts "Тепло (нужно меньше)"
+
+  user_answer1=gets.to_i
+end
+
+if user_answer1>specific_number
+  puts "Тепло (нужно меньше)"
+
+  user_answer1=gets.to_i
+end
+
+if user_answer1==specific_number
+  puts "Ура, вы выиграли!"
+
+else
+  puts "вы не угадали"
+end
+
+
+
+
+puts "Загаданое число было: " + specific_number.to_s
