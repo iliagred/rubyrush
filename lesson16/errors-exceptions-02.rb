1b5c8e331b7af32d9e3c5076c94fb67dd699cc3f
Ilia <iliagred@gmail.com>
2020-11-11 09:36:30 +0200

    error-exceptions-02

---
+++ lesson16/errors-exceptions-02.rb
@@ -0,0 +1,34 @@
+puts "Первое число:"
+first_user_input=STDIN.gets.to_f
+
+puts "Второе число:"
+second_user_input=STDIN.gets.to_f
+
+puts "Выберите операцию (+ - * /):"
+calc_user_input=STDIN.gets.chomp
+
+begin
+  case calc_user_input
+  when '*'
+    puts "Результат: "
+    puts first_user_input * second_user_input
+  when '+'
+    puts "Результат: "
+    puts first_user_input + second_user_input
+  when '-'
+    puts "Результат:"
+    puts first_user_input - second_user_input
+  when '/'
+    puts "Результат:"
+    puts first_user_input / second_user_input
+  end
+rescue ZeroDivisionError #=> error
+  puts "На ноль делить нельзя " #+ error.message
+rescue ZeroDivisionError #=> error
+  puts "На ноль делить нельзя " #+ error.message
+end
+#calc_result=first_user_input calc_user_input second_user_input
+
+#puts "Результат:"
+#puts calc_result
+
